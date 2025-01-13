a95f1b040eb29a8220413b7081f34965c58a6396
Gitudon <oniku.udon.029@gmail.com>
2025-01-13 09:45:27 +0900

    表示タイミングの調整

--- main.py
+++ main.py
@@ -180,23 +180,23 @@ def display_codes():
                         break
     return
 
+def load():
+    while True:
+        try:
+            display_codes()
+        except:
+            st.write("ERROR: 再試行してください。")
+        else:
+            break
+
 def main():
     message()
     input_students()
     input_contents()
     if display_url!="":
         st.write(f'<center><span style="font-size:20px;"><a href="{display_url}">{display_url}</a></span></center>',unsafe_allow_html=True)
-    while True:
-        while True:
-            try:
-                display_codes()
-            except:
-                st.write("ERROR: 再試行してください。")
-            else:
-                break
-        if st.button("再入力"):
-            input_students()
-            input_contents()
+    if st.button("コードを表示"):
+        load()
 
 if __name__=="__main__":
     main()
